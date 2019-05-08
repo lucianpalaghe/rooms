1ce870feac935705d8083558244517fe3477c39d
Lucian <l.palaghe@gmail.com>
2019-05-08 15:09:09 +0300

    Added Id to EquipmentDto, removed redundant collect

--- src/main/java/ro/pss/spring/rooms/RoomReservationsApplication.java
+++ src/main/java/ro/pss/spring/rooms/RoomReservationsApplication.java
@@ -2,7 +2,9 @@ package ro.pss.spring.rooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class RoomReservationsApplication {
 
--- src/main/java/ro/pss/spring/rooms/web/dto/EquipmentDto.java
+++ src/main/java/ro/pss/spring/rooms/web/dto/EquipmentDto.java
@@ -5,10 +5,12 @@ import ro.pss.spring.rooms.model.Equipment;
 
 @NoArgsConstructor
 public class EquipmentDto {
+	public Long id;
 	public String type;
 	public String serialNumber;
 
 	public EquipmentDto(Equipment equipment) {
+		this.id = equipment.getId();
 		this.type = equipment.getType().value();
 		this.serialNumber = equipment.getSerialNumber();
 	}
